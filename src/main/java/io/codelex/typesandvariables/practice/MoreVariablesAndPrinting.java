0bdf55bc34976ced5e5566896a0f43ddf8a06636
Reinis Kalbergs <r.kalbergs@gmail.com>
2022-02-23 14:43:42 +0200

    Completed typesandvariables exercises 1 and 2

---
+++ src/main/java/io/codelex/typesandvariables/practice/MoreVariablesAndPrinting.java
@@ -0,0 +1,31 @@
+package io.codelex.typesandvariables.practice;
+
+public class MoreVariablesAndPrinting {
+    public static void main(String[] args) {
+        String name;
+        String eyes;
+        String teeth;
+        String hair;
+        int age;
+        double height;
+        double weight;
+
+        name = "Zed A. Shaw";
+        age = 35;
+        height = 74 * 2.54;  // inches->cm
+        weight = 180 * 0.453592; // lbs->kg
+        eyes = "Blue";
+        teeth = "White";
+        hair = "Brown";
+
+        System.out.println("Let's talk about " + name + ".");
+        System.out.println("He's " + String.format("%.2f", height) + " cm tall.");
+        System.out.println("He's " + String.format("%.2f", weight) + " kg heavy.");
+        System.out.println("Actually, that's not too heavy.");
+        System.out.println("He's got " + eyes + " eyes and " + hair + " hair.");
+        System.out.println("His teeth are usually " + teeth + " depending on the coffee.");
+
+        System.out.println("If I add " + age + ", " + height + ", and " + String.format("%.2f", weight)
+                + " I get " + String.format("%.2f", age + height + weight) + ".");
+    }
+}
