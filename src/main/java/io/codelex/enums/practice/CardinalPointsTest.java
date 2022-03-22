f279856c8ca3ec1bdfafde3b5fe7853eef966799
Reinis Kalbergs <r.kalbergs@gmail.com>
2022-03-22 15:43:44 +0200

    Completed enum and map exercises

---
+++ src/main/java/io/codelex/enums/practice/CardinalPointsTest.java
@@ -0,0 +1,10 @@
+package io.codelex.enums.practice;
+
+public class CardinalPointsTest {
+    public static void main(String[] args) {
+        CardinalPoints.printDirection(CardinalPoints.North);
+        CardinalPoints.printDirection(CardinalPoints.South);
+        CardinalPoints.printDirection(CardinalPoints.East);
+        CardinalPoints.printDirection(CardinalPoints.West);
+    }
+}
